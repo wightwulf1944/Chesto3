9506ec4404c75172f89c883b503e543b04d3d835
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2017-07-27 17:19:13 +0800

    Changed layoutmanager to flexbox

--- app/src/main/java/i/am/shiro/chesto/activitysearch/SearchActivity.java
+++ app/src/main/java/i/am/shiro/chesto/activitysearch/SearchActivity.java
@@ -6,9 +6,13 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.flexbox.FlexWrap;
+import com.google.android.flexbox.FlexboxLayoutManager;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,6 +25,7 @@ import static butterknife.ButterKnife.findById;
 public class SearchActivity extends AppCompatActivity {
 
     @BindView(R.id.editText) EditText editText;
+    @BindView(R.id.clearButton) ImageButton clearButton;
     private String currentQuery;
     private TagStore tagStore;
 
@@ -38,6 +43,8 @@ public class SearchActivity extends AppCompatActivity {
             supportActionBar.setDisplayShowTitleEnabled(false);
         }
 
+        clearButton.setOnClickListener(view -> editText.setText(""));
+
         EditorSearchListener editorSearchListener = new EditorSearchListener();
         editorSearchListener.setAction(this::invokeSearch);
         editText.setOnEditorActionListener(editorSearchListener);
@@ -49,55 +56,50 @@ public class SearchActivity extends AppCompatActivity {
         SearchAdapter adapter = new SearchAdapter();
         adapter.setOnItemClickListener(this::onAdapterItemClicked);
 
+        FlexboxLayoutManager layoutManager = new FlexboxLayoutManager(this);
+        layoutManager.setFlexWrap(FlexWrap.WRAP);
+
         RecyclerView recyclerView = findById(this, R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
 
         tagStore = new TagStore(adapter);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_search, menu);
-        MenuItem clearButton = menu.findItem(R.id.clear);
-
-        AfterTextChangedListener listener = new AfterTextChangedListener();
-        listener.setAction(s -> clearButton.setVisible(!s.isEmpty()));
-        editText.addTextChangedListener(listener);
 
         String searchString = SearchHistory.current().getSearchString();
         editText.setText(searchString);
-        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-            case R.id.clear:
-                editText.setText("");
-                return true;
-            case R.id.go:
-                invokeSearch();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
 
+
     private void onTextChanged(String s) {
+        if (s.isEmpty()) {
+            clearButton.setVisibility(View.GONE);
+        } else {
+            clearButton.setVisibility(View.VISIBLE);
+        }
+
         int spaceIndex = s.lastIndexOf(" ");
         currentQuery = s.substring(spaceIndex + 1);
         tagStore.searchTags(currentQuery);
     }
+
 
     private void onAdapterItemClicked(String itemName) {
         String text = editText.getText()
                 .toString()
                 .replaceFirst(currentQuery, itemName);
         editText.setText(text);
+        invokeSearch();
     }
 
     private void invokeSearch() {
