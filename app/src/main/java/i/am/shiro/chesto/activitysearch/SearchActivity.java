74095f8e8c4bfe8050c0ad41df0a7fde6df9a21f
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2017-09-28 00:47:18 +0800

    simplified search activity
    kj

--- app/src/main/java/i/am/shiro/chesto/activitysearch/SearchActivity.java
+++ app/src/main/java/i/am/shiro/chesto/activitysearch/SearchActivity.java
@@ -2,56 +2,36 @@ package i.am.shiro.chesto.activitysearch;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
 
 import com.google.android.flexbox.FlexWrap;
 import com.google.android.flexbox.FlexboxLayoutManager;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import i.am.shiro.chesto.R;
 import i.am.shiro.chesto.activitymain2.MainActivity2;
-import i.am.shiro.chesto.engine.SearchHistory;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
-    @BindView(R.id.editText) EditText editText;
-    @BindView(R.id.clearButton) ImageButton clearButton;
     private TagStore tagStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        ButterKnife.bind(this);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar supportActionBar = getSupportActionBar();
-        if (supportActionBar != null) {
-            supportActionBar.setDisplayHomeAsUpEnabled(true);
-            supportActionBar.setDisplayShowTitleEnabled(false);
-        }
+        toolbar.setNavigationIcon(R.drawable.ic_nav_back);
+        toolbar.setNavigationOnClickListener(view -> finish());
 
-        clearButton.setOnClickListener(view -> editText.setText(""));
-
-        EditorSearchListener editorSearchListener = new EditorSearchListener();
-        editorSearchListener.setAction(this::invokeSearch);
-        editText.setOnEditorActionListener(editorSearchListener);
-
-        AfterTextChangedListener afterTextChangedListener = new AfterTextChangedListener();
-        afterTextChangedListener.setAction(this::onTextChanged);
-        editText.addTextChangedListener(afterTextChangedListener);
+        SearchView searchView = findViewById(R.id.searchView);
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setOnQueryTextListener(this);
 
         SearchAdapter adapter = new SearchAdapter();
-        adapter.setOnItemClickListener(this::invokeSearch);
+        adapter.setOnItemClickListener(this::onQueryTextSubmit);
 
         FlexboxLayoutManager layoutManager = new FlexboxLayoutManager(this);
         layoutManager.setFlexWrap(FlexWrap.WRAP);
@@ -62,39 +42,24 @@ public class SearchActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         tagStore = new TagStore(adapter);
-
-        String searchString = SearchHistory.current().getSearchString();
-        editText.setText(searchString);
+        tagStore.searchTags("");
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public boolean onQueryTextSubmit(String s) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.setAction(Intent.ACTION_SEARCH);
+        intent.putExtra("default", s);
+        startActivity(intent);
+        finish();
+        return true;
     }
 
-
-    private void onTextChanged(String s) {
-        if (s.isEmpty()) {
-            clearButton.setVisibility(View.GONE);
-        } else {
-            clearButton.setVisibility(View.VISIBLE);
-        }
-
+    @Override
+    public boolean onQueryTextChange(String s) {
         int spaceIndex = s.lastIndexOf(" ");
         String currentQuery = s.substring(spaceIndex + 1);
         tagStore.searchTags(currentQuery);
-    }
-
-    private void invokeSearch(String searchString) {
-        Intent intent = new Intent(this, MainActivity2.class);
-        intent.setAction(Intent.ACTION_SEARCH);
-        intent.putExtra("default", searchString);
-        startActivity(intent);
-        finish();
+        return true;
     }
 }
